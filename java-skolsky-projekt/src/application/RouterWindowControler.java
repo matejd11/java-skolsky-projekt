f425f33308b40d74ce29fae526439fa74f3966e3
MatejD <mdujava@gmail.com>
2015-04-25 12:23:07 +0200

    refresh unmark password as promttext

--- java-skolsky-projekt/src/application/RouterWindowControler.java
+++ java-skolsky-projekt/src/application/RouterWindowControler.java
@@ -5,16 +5,29 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import Serial.SerialPortHelper;
 
 public class RouterWindowControler {
 
 	@FXML
-	private Button	RefreshButton;
+	private Button			RefreshButton;
+
+	@FXML
+	private PasswordField	passwordField;
+
+	@FXML
+	private TextField		textField;
 
 	@FXML
 	void Button_click_refresh(ActionEvent event) throws IOException {
 		System.out.println("skuska");
 		SerialPortHelper.send("skuska");
+		System.out.println(passwordField.getText());
+
+		String a = passwordField.getText();
+		passwordField.clear();
+		passwordField.setPromptText(a);
 	}
 }
